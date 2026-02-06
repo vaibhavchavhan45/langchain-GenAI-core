0797ee5d739f93cceba49bf27b1fd17c602fe506
Vaibhav Chavhan <chavhanvaibhav708@gmail.com>
2026-02-06 04:37:20 +0000

    Add JSON schema for structured output

---
+++ 04_structured_output/08_json_schema.py
@@ -0,0 +1,12 @@
+{
+    "title" : "Student",
+    "description" : "Schema for students",
+    "type": "object",
+    "propreties" : {
+        "name" : "string",
+        "roll_no" : "integer",
+        "age" : "integer",
+        "class" : "string"
+    },
+    "required" : ['name', 'roll_no']
+}
